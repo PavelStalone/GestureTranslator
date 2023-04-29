13e0d140284d9703a7df23bc2a8272510a00fbce
PoroshinGA <119220049+PoroshinGA@users.noreply.github.com>
2023-04-29 14:03:33 +0300

    Make the opening of the menu by the button

--- app/src/main/java/com/example/gesturetranslator/presentation/MainActivity.java
+++ app/src/main/java/com/example/gesturetranslator/presentation/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.gesturetranslator.presentation;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.gesturetranslator.R;
 import com.example.gesturetranslator.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -29,10 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
         Constant.MAIN = this;
 
-        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navigateToMainFrame);
         navController = navHostFragment.getNavController();
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//ВОТ ОТ СЮДА
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout_id);
+
+        ImageButton menuButton = findViewById(R.id.menu_btn);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
+        });
+//    ДО СЮДА)
     }
+
 
 }
