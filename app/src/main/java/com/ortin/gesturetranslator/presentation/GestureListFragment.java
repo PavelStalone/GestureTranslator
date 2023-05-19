3aee48fc8e7b9e938c1c3d45949cd48e8b28b55d
PoroshinGA <119220049+PoroshinGA@users.noreply.github.com>
2023-05-20 00:59:45 +0300

    create new UI

--- app/src/main/java/com/ortin/gesturetranslator/presentation/GestureListFragment.java
+++ app/src/main/java/com/ortin/gesturetranslator/presentation/GestureListFragment.java
@@ -1,6 +1,32 @@
-package com.example.gesturetranslator.presentation;
+package com.ortin.gesturetranslator.presentation;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class GestureListFragment extends Activity {
+import androidx.fragment.app.Fragment;
+
+import com.ortin.gesturetranslator.databinding.GestureListLayoutBinding;
+import com.ortin.gesturetranslator.databinding.InformationLayoutBinding;
+
+public class GestureListFragment extends Fragment {
+    GestureListLayoutBinding binding;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = GestureListLayoutBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
