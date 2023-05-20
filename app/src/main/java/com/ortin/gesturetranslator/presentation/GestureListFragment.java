3b2d6d514fab1f4021062c5ce20e7b0cf6e6c773
PavelStalone <pavel.shoplik@yandex.ru>
2023-05-20 23:25:03 +0300

    Changed navGraph. Renamed drawableResourses. Changed SettingsFragment.

--- app/src/main/java/com/ortin/gesturetranslator/presentation/GestureListFragment.java
+++ app/src/main/java/com/ortin/gesturetranslator/presentation/GestureListFragment.java
@@ -1,24 +1,20 @@
 package com.ortin.gesturetranslator.presentation;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.ortin.gesturetranslator.databinding.GestureListLayoutBinding;
-import com.ortin.gesturetranslator.databinding.InformationLayoutBinding;
 
 public class GestureListFragment extends Fragment {
     GestureListLayoutBinding binding;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = GestureListLayoutBinding.inflate(inflater, container, false);
         return binding.getRoot();
--- app/src/main/java/com/ortin/gesturetranslator/presentation/SettingsFragment.java
+++ app/src/main/java/com/ortin/gesturetranslator/presentation/SettingsFragment.java
@@ -6,26 +6,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
-import com.ortin.gesturetranslator.databinding.GestureListLayoutBinding;
 import com.ortin.gesturetranslator.databinding.SettingsLayoutBinding;
 
 public class SettingsFragment extends Fragment implements CompoundButton.OnCheckedChangeListener {
     private SettingsLayoutBinding binding;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        SwitchCompat switchCompat = binding.switchCompat;
-        switchCompat.setOnCheckedChangeListener(this);
-    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = SettingsLayoutBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.switchCompat.setOnCheckedChangeListener(this);
     }
 
     @Override
@@ -36,6 +39,7 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//        ТУТ ВРОДЕ КАК ПЕРЕКЛЮЧЕНИЕ ТЕМЫ
+        if (isChecked)AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 }
