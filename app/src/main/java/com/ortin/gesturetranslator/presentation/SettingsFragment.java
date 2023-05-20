c86b9f328afdf5cf443c86ae68decd42dc4fe8a9
PoroshinGA <119220049+PoroshinGA@users.noreply.github.com>
2023-05-21 01:34:45 +0300

    create seekBar

--- app/src/main/java/com/ortin/gesturetranslator/presentation/SettingsFragment.java
+++ app/src/main/java/com/ortin/gesturetranslator/presentation/SettingsFragment.java
@@ -1,10 +1,13 @@
 package com.ortin.gesturetranslator.presentation;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +15,10 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
+import com.ortin.gesturetranslator.R;
 import com.ortin.gesturetranslator.databinding.SettingsLayoutBinding;
 
-public class SettingsFragment extends Fragment implements CompoundButton.OnCheckedChangeListener {
+public class SettingsFragment extends Fragment implements CompoundButton.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener{
     private SettingsLayoutBinding binding;
 
     @Override
@@ -28,7 +32,13 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.switchCompat.setOnCheckedChangeListener(this);
+        binding.switchCompatTheme.setOnCheckedChangeListener(this);
+        binding.switchCompatGpu.setOnCheckedChangeListener(this);
+        binding.switchCompatPercents.setOnCheckedChangeListener(this);
+//        ТАК ИДЕЯ СКАЗАЛА НУЖНО |
+        binding.seekBar.setOnSeekBarChangeListener((SeekBar.OnSeekBarChangeListener) this);
+        binding.seekBar.getProgressDrawable().setColorFilter(getContext().getColor(R.color.element_text_color_day), PorterDuff.Mode.SRC_ATOP);
+        binding.seekBar.getThumb().setColorFilter(getContext().getColor(R.color.element_text_color_day), PorterDuff.Mode.SRC_ATOP);
     }
 
     @Override
@@ -41,5 +51,19 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (isChecked)AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        binding.seekBarText.setText(String.valueOf(seekBar.getProgress()));
     }
 }
