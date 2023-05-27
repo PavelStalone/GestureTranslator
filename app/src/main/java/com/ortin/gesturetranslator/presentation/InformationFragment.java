ebeb4f89267b0cb756e7f2399a38b434e69deaf7
PoroshinGA <119220049+PoroshinGA@users.noreply.github.com>
2023-05-27 15:31:18 +0300

    changed information layout, added hand logo and custom button

--- app/src/main/java/com/ortin/gesturetranslator/presentation/InformationFragment.java
+++ app/src/main/java/com/ortin/gesturetranslator/presentation/InformationFragment.java
@@ -1,5 +1,7 @@
 package com.ortin.gesturetranslator.presentation;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +26,48 @@ public class InformationFragment extends Fragment {
         binding = InformationFragmentBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+    private void goToWebApplication(){
+        goToUrl("будет ссылка");
+    }
+
+    private void goToAppGithub(){
+        goToUrl("https://github.com/PavelStalone/GestureTranslator");
+    }
+    private void goToPavelStaloneGithub(){
+        goToUrl("https://github.com/PavelStalone");
+    }
+    private void goToPavelStaloneContacts(){}
+
+    private void goToGlebixGithub(){
+        goToUrl("https://github.com/PoroshinGA");
+    }
+    private void goToGlebixContacts(){}
+
+    private void goToQondeeterGithub(){
+        goToUrl("https://github.com/kond1ter");
+    }
+    private void goToQondeeterContacts(){}
+
+    private void goToSovaGithub(){
+        goToUrl("https://github.com/N1kySSS");
+    }
+    private void goToSovaContacts(){}
+
+    private void goToGlazeezalgGithub(){
+        goToUrl("https://github.com/glazeezalg");
+    }
+    private void goToGlazeezalgContacts(){}
+
+    private void goToSeverGithub(){
+        goToUrl("https://github.com/jacksever");
+    }
+    private void goToSeverContacts(){}
 
     @Override
     public void onDestroyView() {
