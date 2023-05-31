25aa603fdd2e9be9b4775efb9ba66adab8c547b2
PoroshinGA <119220049+PoroshinGA@users.noreply.github.com>
2023-05-31 23:07:30 +0300

    Changed the color of the selected item in the left menu, added image_from_gallery_layout.xml and ImageFromGalleryFragment

---
+++ app/src/main/java/com/ortin/gesturetranslator/presentation/ImageFromGalleryFragment.java
@@ -0,0 +1,39 @@
+package com.ortin.gesturetranslator.presentation;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ortin.gesturetranslator.databinding.ImageFromGalleryLayoutBinding;
+
+public class ImageFromGalleryFragment extends Fragment {
+    ImageFromGalleryLayoutBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = ImageFromGalleryLayoutBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
