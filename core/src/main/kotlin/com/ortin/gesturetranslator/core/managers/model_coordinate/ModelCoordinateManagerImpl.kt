6fd0643db74bd9753815a846db08801c2f986860
Gleb Poroshin <Poroshin.info@yandex.ru>
2023-10-03 16:10:57 +0300

    #33 - Remove Logs, add private modificator to LABELS, use forEachIndexed instead of for

--- core/src/main/kotlin/com/ortin/gesturetranslator/core/managers/model_coordinate/ModelCoordinateManagerImpl.kt
+++ core/src/main/kotlin/com/ortin/gesturetranslator/core/managers/model_coordinate/ModelCoordinateManagerImpl.kt
@@ -4,9 +4,8 @@ import android.util.Log
 import com.ortin.gesturetranslator.core.managers.model_coordinate.ml_models.MlModel
 import com.ortin.gesturetranslator.core.managers.model_coordinate.models.ModelCoordinateArray
 import com.ortin.gesturetranslator.core.managers.model_coordinate.models.ModelCoordinateClassificationArray
-
 class ModelCoordinateManagerImpl : ModelCoordinateManager {
-    val LABELS = arrayOf(
+    private val LABELS = arrayOf(
         "А",
         "Б",
         "В",
@@ -37,19 +36,15 @@ class ModelCoordinateManagerImpl : ModelCoordinateManager {
         "Ю",
         "Я"
     )
-
     override fun recognise(modelCoordinateArray: ModelCoordinateArray): ModelCoordinateClassificationArray {
-        Log.e("recognise", "recognise: " + modelCoordinateArray.coordinate.toString())
         val results: DoubleArray = MlModel.score(modelCoordinateArray.coordinate)
         var maxScore = 0.0
         var maxIndex = 0
 
-        Log.e("recognise", "results: $results")
-
-        for (i in results.indices) {
+        results.forEachIndexed { i, _ ->
             if (results[i] > maxScore) {
-                maxScore = results[i]
-                maxIndex = i
+            maxScore = results[i]
+            maxIndex = i
             }
         }
         return ModelCoordinateClassificationArray((maxScore * 100).toFloat(), LABELS[maxIndex])
