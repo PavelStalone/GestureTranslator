a47627ab41314c41d18cfdded55cc1d0660eb723
Gleb Poroshin <Poroshin.info@yandex.ru>
2023-10-05 11:52:50 +0300

    #33 - Remove Labels from Companion Object

--- core/src/main/kotlin/com/ortin/gesturetranslator/core/managers/model_coordinate/ModelCoordinateManagerImpl.kt
+++ core/src/main/kotlin/com/ortin/gesturetranslator/core/managers/model_coordinate/ModelCoordinateManagerImpl.kt
@@ -5,6 +5,38 @@ import com.ortin.gesturetranslator.core.managers.model_coordinate.models.ModelCo
 import com.ortin.gesturetranslator.core.managers.model_coordinate.models.ModelCoordinateClassificationArray
 
 class ModelCoordinateManagerImpl : ModelCoordinateManager {
+    private val LABELS = listOf(
+        "А",
+        "Б",
+        "В",
+        "Г",
+        "Д",
+        "Е",
+        "Ж",
+        "З",
+        "И",
+        "К",
+        "Л",
+        "М",
+        "Н",
+        "О",
+        "П",
+        "Р",
+        "С",
+        "Т",
+        "У",
+        "Ф",
+        "Х",
+        "Ц",
+        "Ч",
+        "Ш",
+        "Ы",
+        "Ь",
+        "Э",
+        "Ю",
+        "Я"
+    )
+
     override fun recognise(modelCoordinateArray: ModelCoordinateArray): ModelCoordinateClassificationArray {
         val results: List<Double> = MlModel.score(modelCoordinateArray.coordinate)
         var maxScore = 0.0
@@ -17,39 +49,5 @@ class ModelCoordinateManagerImpl : ModelCoordinateManager {
             }
         }
         return ModelCoordinateClassificationArray((maxScore * 100).toFloat(), LABELS[maxIndex])
-    }
-
-    companion object {
-        private val LABELS = listOf(
-            "А",
-            "Б",
-            "В",
-            "Г",
-            "Д",
-            "Е",
-            "Ж",
-            "З",
-            "И",
-            "К",
-            "Л",
-            "М",
-            "Н",
-            "О",
-            "П",
-            "Р",
-            "С",
-            "Т",
-            "У",
-            "Ф",
-            "Х",
-            "Ц",
-            "Ч",
-            "Ш",
-            "Ы",
-            "Ь",
-            "Э",
-            "Ю",
-            "Я"
-        )
     }
 }
