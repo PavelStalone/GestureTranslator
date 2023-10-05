305b75658c7cd53ad97a807c6a0bddac65fba8f2
Gleb Poroshin <Poroshin.info@yandex.ru>
2023-10-05 11:01:33 +0300

    #33 - Fixed bugs and suggestions

--- core/src/main/kotlin/com/ortin/gesturetranslator/core/managers/model_coordinate/ModelCoordinateManagerImpl.kt
+++ core/src/main/kotlin/com/ortin/gesturetranslator/core/managers/model_coordinate/ModelCoordinateManagerImpl.kt
@@ -1,52 +1,55 @@
 package com.ortin.gesturetranslator.core.managers.model_coordinate
 
-import android.util.Log
 import com.ortin.gesturetranslator.core.managers.model_coordinate.ml_models.MlModel
 import com.ortin.gesturetranslator.core.managers.model_coordinate.models.ModelCoordinateArray
 import com.ortin.gesturetranslator.core.managers.model_coordinate.models.ModelCoordinateClassificationArray
+
 class ModelCoordinateManagerImpl : ModelCoordinateManager {
-    private val LABELS = arrayOf(
-        "А",
-        "Б",
-        "В",
-        "Г",
-        "Д",
-        "Е",
-        "Ж",
-        "З",
-        "И",
-        "К",
-        "Л",
-        "М",
-        "Н",
-        "О",
-        "П",
-        "Р",
-        "С",
-        "Т",
-        "У",
-        "Ф",
-        "Х",
-        "Ц",
-        "Ч",
-        "Ш",
-        "Ы",
-        "Ь",
-        "Э",
-        "Ю",
-        "Я"
-    )
     override fun recognise(modelCoordinateArray: ModelCoordinateArray): ModelCoordinateClassificationArray {
-        val results: DoubleArray = MlModel.score(modelCoordinateArray.coordinate)
+        val results: List<Double> = MlModel.score(modelCoordinateArray.coordinate)
         var maxScore = 0.0
         var maxIndex = 0
 
         results.forEachIndexed { i, _ ->
             if (results[i] > maxScore) {
-            maxScore = results[i]
-            maxIndex = i
+                maxScore = results[i]
+                maxIndex = i
             }
         }
         return ModelCoordinateClassificationArray((maxScore * 100).toFloat(), LABELS[maxIndex])
+    }
+
+    companion object {
+        private val LABELS = listOf(
+            "А",
+            "Б",
+            "В",
+            "Г",
+            "Д",
+            "Е",
+            "Ж",
+            "З",
+            "И",
+            "К",
+            "Л",
+            "М",
+            "Н",
+            "О",
+            "П",
+            "Р",
+            "С",
+            "Т",
+            "У",
+            "Ф",
+            "Х",
+            "Ц",
+            "Ч",
+            "Ш",
+            "Ы",
+            "Ь",
+            "Э",
+            "Ю",
+            "Я"
+        )
     }
 }
--- core/src/main/kotlin/com/ortin/gesturetranslator/core/managers/model_coordinate/models/ModelCoordinateArray.kt
+++ core/src/main/kotlin/com/ortin/gesturetranslator/core/managers/model_coordinate/models/ModelCoordinateArray.kt
@@ -1,13 +1,3 @@
 package com.ortin.gesturetranslator.core.managers.model_coordinate.models
 
- data class ModelCoordinateArray (val coordinate: DoubleArray) {
-     override fun equals(other: Any?): Boolean {
-         if (this === other) return true
-         if (javaClass != other?.javaClass) return false
-
-         other as ModelCoordinateArray
-         return coordinate.contentEquals(other.coordinate)
-     }
-
-     override fun hashCode(): Int = coordinate.contentHashCode()
- }
+data class ModelCoordinateArray(val coordinate: List<Double>)
