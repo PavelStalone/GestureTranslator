27287b4e52effb44aa096641d9cda275a420f684
Pavel Shoplik <pavel.shoplik@yandex.ru>
2023-10-06 13:58:00 +0300

    #50 - Remove YUVtoRGB. Added BitmapAnalyzer. Changed CameraManager.

--- feature/src/main/kotlin/com/ortin/gesturetranslator/feature/managers/camera/CameraManager.kt
+++ feature/src/main/kotlin/com/ortin/gesturetranslator/feature/managers/camera/CameraManager.kt
@@ -1,9 +1,10 @@
 package com.ortin.gesturetranslator.feature.managers.camera
 
+import androidx.camera.core.CameraSelector
 import androidx.lifecycle.LifecycleOwner
 import com.ortin.gesturetranslator.feature.managers.camera.listeners.CameraListener
 
 interface CameraManager {
-    fun loadImage(cameraListener: CameraListener?, lifecycleOwner: LifecycleOwner?)
+    fun loadImage(cameraListener: CameraListener, lifecycleOwner: LifecycleOwner, cameraFacing: Int = CameraSelector.LENS_FACING_BACK)
     fun setStatusFlashlight(mode: Boolean)
 }
