ad14919edebd2dd73b8e698af8f1bd9a628cb3a7
Nikita Savvin <nik.savvin.2004@mail.ru>
2023-10-09 00:29:57 +0300

    #27 - Fixed indents

--- domain/src/main/kotlin/com/ortin/gesturetranslator/domain/models/SettingsDomain.kt
+++ domain/src/main/kotlin/com/ortin/gesturetranslator/domain/models/SettingsDomain.kt
@@ -1,8 +1,8 @@
 package com.ortin.gesturetranslator.domain.models
 
 data class SettingsDomain(
-        val theme: Boolean = false,
-        val gpu: Boolean = false,
-        val percent: Boolean = false,
-        val speedFrameDetection: Int = 0
+    val theme: Boolean = false,
+    val gpu: Boolean = false,
+    val percent: Boolean = false,
+    val speedFrameDetection: Int = 0
 )
