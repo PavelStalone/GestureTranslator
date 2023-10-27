f6bd64226456de36629091176ed483edabcd6e80
Pavel Shoplik <pavel.shoplik@yandex.ru>
2023-10-27 23:27:48 +0300

    #76 - Added ksp in Room

--- build-logic/convention/src/main/kotlin/AndroidRoomConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidRoomConventionPlugin.kt
@@ -6,10 +6,14 @@ import org.gradle.kotlin.dsl.dependencies
 class AndroidRoomConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
+            with(pluginManager) {
+                apply("com.google.devtools.ksp")
+            }
+
             dependencies {
                 add("implementation", libs.findLibrary("androidx-room-runtime").get())
-                add("implementation", libs.findLibrary("androidx-room-compiler").get())
                 add("implementation", libs.findLibrary("androidx-room-ktx").get())
+                add("ksp", libs.findLibrary("androidx-room-compiler").get())
                 add("testImplementation", libs.findLibrary("androidx-room-testing").get())
             }
         }
