f1c01798b38f5de2fe3bd34dcafdd1aaedaf3923
Nikita Savvin <nik.savvin.2004@mail.ru>
2023-11-07 15:16:02 +0300

    #89 - Create component for recognized letter

---
+++ ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/RecognizedLetter.kt
@@ -0,0 +1,39 @@
+package com.ortin.gesturetranslator.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun RecognizedLetter(
+    letter: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = letter,
+        modifier = modifier
+            .clip(shape = RoundedCornerShape(12.dp))
+            .background(MaterialTheme.colorScheme.primary),
+        textAlign = TextAlign.Center,
+        fontSize = 20.sp,
+        color = MaterialTheme.colorScheme.onPrimary
+    )
+}
+
+@Preview
+@Composable
+fun PreviewRecognizedLetter() {
+    RecognizedLetter(
+        letter = "A",
+        modifier = Modifier.width(48.dp)
+    )
+}
