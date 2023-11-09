9090d2134e593d96e35a9a45105b1e6ea2ece61b
Nikita Savvin <nik.savvin.2004@mail.ru>
2023-11-09 23:58:53 +0300

    #90 - try to make gesture card

---
+++ ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/cards/GestureCard.kt
@@ -0,0 +1,49 @@
+package com.ortin.gesturetranslator.ui.components.cards
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ortin.gesturetranslator.ui.R
+
+@Composable
+fun GestureCard(
+    letter: String,
+    @DrawableRes id: Int,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .size(140.dp)
+            .clip(MaterialTheme.shapes.medium),
+
+    ) {
+        Text(
+            text = letter,
+            modifier = modifier
+                .size(40.dp)
+                .clip(MaterialTheme.shapes.medium)
+                .background(MaterialTheme.colorScheme.primary),
+            style = MaterialTheme.typography.headlineSmall,
+            color = MaterialTheme.colorScheme.onPrimary,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview
+@Composable
+fun GestureCardPreview() {
+    GestureCard(
+        letter = "A",
+        id = R.drawable.a
+    )
+}
