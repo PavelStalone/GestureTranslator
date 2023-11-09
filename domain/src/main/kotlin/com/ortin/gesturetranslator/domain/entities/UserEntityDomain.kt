8e16991e09aae7d5b2edacfbe17883be6cbb3f4b
Niktia Vereschagin <nikitavereshagin2@gmail.com>
2023-11-10 01:09:51 +0300

    #37 Removed DataModule

--- domain/src/main/kotlin/com/ortin/gesturetranslator/domain/entities/UserEntityDomain.kt
+++ domain/src/main/kotlin/com/ortin/gesturetranslator/domain/entities/UserEntityDomain.kt
@@ -1,6 +1,7 @@
 package com.ortin.gesturetranslator.domain.entities
 
 data class UserEntityDomain(
+    val id: Int = 0,
     val login: String,
     val password: String,
     val isSigned: Boolean = false
