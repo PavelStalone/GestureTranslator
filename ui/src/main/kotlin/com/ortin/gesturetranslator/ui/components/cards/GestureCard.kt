7c4b3faa1e5c8cbbd3e2f69301ef241b4beacc9b
Nikita Savvin <nik.savvin.2004@mail.ru>
2023-11-10 00:59:43 +0300

    #90 - Modify gesture card

--- ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/cards/GestureCard.kt
+++ ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/cards/GestureCard.kt
@@ -1,14 +1,19 @@
 package com.ortin.gesturetranslator.ui.components.cards
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,24 +22,33 @@ import com.ortin.gesturetranslator.ui.R
 @Composable
 fun GestureCard(
     letter: String,
-    @DrawableRes id: Int,
+    @DrawableRes imageId: Int,
     modifier: Modifier = Modifier
 ) {
     Card(
         modifier = modifier
-            .size(140.dp)
-            .clip(MaterialTheme.shapes.medium),
+            .size(140.dp),
+        shape = MaterialTheme.shapes.medium
     ) {
-        Text(
-            text = letter,
-            modifier = modifier
-                .size(40.dp)
-                .clip(MaterialTheme.shapes.medium)
-                .background(MaterialTheme.colorScheme.primary),
-            style = MaterialTheme.typography.headlineSmall,
-            color = MaterialTheme.colorScheme.onPrimary,
-            textAlign = TextAlign.Center
-        )
+        Box(
+            modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomStart
+        ) {
+            Image(
+                painter = painterResource(id = imageId),
+                contentDescription = letter
+            )
+            Text(
+                text = letter,
+                modifier = modifier
+                    .size(40.dp)
+                    .clip(MaterialTheme.shapes.medium)
+                    .background(MaterialTheme.colorScheme.primary),
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onPrimary,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
 
@@ -43,6 +57,6 @@ fun GestureCard(
 fun GestureCardPreview() {
     GestureCard(
         letter = "A",
-        id = R.drawable.a
+        imageId = R.drawable.a
     )
 }
