ab5021ef9cf41c516959806c9384ff0be37b66fe
Gleb Poroshin <Poroshin.info@yandex.ru>
2023-11-20 01:38:17 +0300

    #92 - Create DeveloperCard

--- ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/buttons/SecondaryTextButton.kt
+++ ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/buttons/SecondaryTextButton.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.ortin.gesturetranslator.ui.theme.GestureTranslatorTheme
 
 @Composable
@@ -55,7 +54,7 @@ fun SecondaryTextButton(
                 text = text,
                 modifier = Modifier.weight(1f),
                 textAlign = TextAlign.Center,
-                fontSize = 20.sp
+                style = MaterialTheme.typography.titleSmall
             )
             rightIcon?.let {
                 Icon(
--- ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/cards/DeveloperCard.kt
+++ ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/cards/DeveloperCard.kt
@@ -1,2 +1,152 @@
 package com.ortin.gesturetranslator.ui.components.cards
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ortin.gesturetranslator.ui.R
+import com.ortin.gesturetranslator.ui.components.buttons.PrimaryTextButton
+import com.ortin.gesturetranslator.ui.theme.GestureTranslatorTheme
+import com.ortin.gesturetranslator.ui.theme.LocalDimensions
+import com.ortin.gesturetranslator.ui.theme.surfaceContainerLow
+import timber.log.Timber
+
+
+@Composable
+fun DeveloperCard(
+    title: String,
+    description: String,
+    gitHubUrl: String?,
+    email: String?,
+    modifier: Modifier  = Modifier,
+    iconId: Int = R.drawable.icon_ortin_logo_without_text
+) {
+    val localDimensions = LocalDimensions.current
+    val uriHandler = LocalUriHandler.current
+    val context = LocalContext.current
+
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = surfaceContainerLow
+        )
+    ) {
+        Spacer(modifier = Modifier.height(localDimensions.verticalStandard))
+
+        Text(
+            modifier = Modifier.padding(horizontal = localDimensions.horizontalMedium),
+            text = title,
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight(600),
+                fontSize = 24.sp
+            )
+        )
+
+        Text(
+            modifier = Modifier.padding(horizontal = localDimensions.horizontalMedium),
+            text = description,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            style = MaterialTheme.typography.bodyMedium
+        )
+
+        Row(
+            modifier = Modifier.padding(
+                horizontal = localDimensions.horizontalMedium,
+                vertical = localDimensions.verticalTiny
+            ),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = iconId),
+                contentDescription = null
+            )
+
+            gitHubUrl?.let {
+                Spacer(modifier = Modifier.width(localDimensions.horizontalSmall))
+                PrimaryTextButton(
+                    text = stringResource(id = R.string.github),
+                    onClick = {
+                        uriHandler.openUri(gitHubUrl)
+                        Timber.d("Go to $title GitHub account")
+                    },
+                    modifier = Modifier
+                        .height(localDimensions.verticalXlarge)
+                        .weight(0.4f)
+                )
+            }
+
+            email?.let {
+                Spacer(modifier = Modifier.width(localDimensions.horizontalSmall))
+                PrimaryTextButton(
+                    text = stringResource(id = R.string.email),
+                    onClick = {
+                        context.openEmailApp(email)
+                        Timber.d("Email app was opened")
+                    },
+                    modifier = Modifier
+                        .height(localDimensions.verticalXlarge)
+                        .weight(0.4f)
+                )
+            }
+
+        }
+    }
+}
+
+/**
+ * Method for opening email app
+ *
+ * @param [email] email address, to which will be open sending email page
+ */
+fun Context.openEmailApp(email: String) {
+    try {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "vnd.android.cursor.item/email"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        startActivity(intent)
+    } catch (e: ActivityNotFoundException) {
+        Timber.e("No email app is available")
+    }
+}
+
+@Preview
+@Composable
+fun DeveloperCardPreview() {
+    GestureTranslatorTheme {
+        Surface {
+            DeveloperCard(
+                modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                title = "PavelStalone",
+                description = "Вообще красавчик машина убийца",
+                gitHubUrl = "null",
+                email = "null"
+            )
+        }
+    }
+}
