e6dbfb0b28a94f8b410a53368bb08c2b8fb8f4fb
Nikita Savvin <nik.savvin.2004@mail.ru>
2023-11-28 16:41:46 +0300

    #103 - Change gesture card

--- ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/cards/GestureCard.kt
+++ ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/cards/GestureCard.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,40 +13,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ortin.gesturetranslator.ui.R
 import com.ortin.gesturetranslator.ui.theme.GestureTranslatorTheme
 
 @Composable
 fun GestureCard(
-    letter: String,
-    @DrawableRes imageId: Int,
-    modifier: Modifier = Modifier
+    letter: String, @DrawableRes imageId: Int, modifier: Modifier = Modifier
 ) {
-    Card(
+    Box(
         modifier = modifier,
-        shape = MaterialTheme.shapes.medium
+        contentAlignment = Alignment.BottomStart
     ) {
+        Image(
+            painter = painterResource(id = imageId),
+            contentDescription = letter,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(MaterialTheme.shapes.medium),
+            contentScale = ContentScale.Crop
+        )
         Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.BottomStart
+            modifier = Modifier
+                .size(40.dp)
+                .clip(MaterialTheme.shapes.medium)
+                .background(MaterialTheme.colorScheme.primary),
+            contentAlignment = Alignment.Center
         ) {
-            Image(
-                painter = painterResource(id = imageId),
-                contentDescription = letter
-            )
             Text(
                 text = letter,
-                modifier = Modifier
-                    .fillMaxSize(0.27f)
-                    .clip(MaterialTheme.shapes.medium)
-                    .background(MaterialTheme.colorScheme.primary),
-                style = MaterialTheme.typography.headlineSmall,
                 color = MaterialTheme.colorScheme.onPrimary,
-                textAlign = TextAlign.Center
+                style = MaterialTheme.typography.headlineSmall.copy(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
             )
         }
     }
