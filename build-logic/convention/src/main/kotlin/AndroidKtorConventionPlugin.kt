3d81ede6e4b15dba5d8ed05f11ebd2f8eed8a4a5
Pavel Shoplik <pavel.shoplik@yandex.ru>
2023-11-30 18:49:50 +0300

    #120 - Added new libraries. Renamed ktor version

--- build-logic/convention/src/main/kotlin/AndroidKtorConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidKtorConventionPlugin.kt
@@ -6,9 +6,20 @@ import org.gradle.kotlin.dsl.dependencies
 class AndroidKtorConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
+            with(pluginManager) {
+                apply("org.jetbrains.kotlin.plugin.serialization")
+            }
+
             dependencies {
                 add("implementation", libs.findLibrary("ktor-client-core").get())
                 add("implementation", libs.findLibrary("ktor-client-cio").get())
+                add("implementation", libs.findLibrary("ktor-client-okhttp").get())
+                add("implementation", libs.findLibrary("ktor-client-android").get())
+                add("implementation", libs.findLibrary("ktor-client-logging").get())
+                add("implementation", libs.findLibrary("ktor-client-serialization").get())
+                add("implementation", libs.findLibrary("ktor-client-content-negotiation").get())
+                add("implementation", libs.findLibrary("ktor-serialization-kotlinx-json").get())
+                add("implementation", libs.findLibrary("kotlinx-serialization-json").get())
             }
         }
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.serialization) apply false
     alias(libs.plugins.google.dagger.hilt) apply false
     alias(libs.plugins.androidx.navigation.safeargs) apply false
     alias(libs.plugins.org.gradle.android.cache.fix) apply false
