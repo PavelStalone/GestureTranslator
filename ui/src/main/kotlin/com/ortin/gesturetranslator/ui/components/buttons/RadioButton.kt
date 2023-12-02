a203dc4a3bc4637b7165fde9b2aa40304b9297db
Gleb Poroshin <Poroshin.info@yandex.ru>
2023-12-02 13:46:32 +0300

    #116 - Remove Row from button, change padding values, add callback to onClick, add isButtonClicked parameter

--- ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/buttons/RadioButton.kt
+++ ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/buttons/RadioButton.kt
@@ -4,7 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -28,20 +28,21 @@ import com.ortin.gesturetranslator.ui.theme.LocalDimensions
 @Composable
 fun RadioButton(
     text: String,
-    onClick: () -> Unit,
+    onClick: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
     isEnabled: Boolean = true,
+    isButtonClicked: Boolean = false,
     @DrawableRes leftIcon: Int? = null,
     @DrawableRes rightIcon: Int? = null
 ) {
     val localDimensions = LocalDimensions.current
-    var isClicked by remember { mutableStateOf(false) }
+    var isClicked by remember { mutableStateOf(isButtonClicked) }
     val borderStroke = 2.dp
 
     Button(
         onClick = {
             isClicked = !isClicked
-            onClick()
+            onClick(isClicked)
         },
         modifier = modifier,
         enabled = isEnabled,
@@ -49,7 +50,10 @@ fun RadioButton(
             width = borderStroke,
             color = MaterialTheme.colorScheme.primary
         ),
-        contentPadding = PaddingValues(vertical = localDimensions.verticalSmall),
+        contentPadding = PaddingValues(
+            vertical = localDimensions.verticalSmall,
+            horizontal = localDimensions.horizontalMedium
+        ),
         shape = RoundedCornerShape(size = localDimensions.buttonsCornerRadius),
         colors = ButtonDefaults.buttonColors(
             containerColor = if (isClicked) {
@@ -62,25 +66,24 @@ fun RadioButton(
             disabledContainerColor = MaterialTheme.colorScheme.surface
         )
     ) {
-        Row {
-            leftIcon?.let {
-                Icon(
-                    painter = painterResource(id = it),
-                    contentDescription = null
-                )
-            }
-            Text(
-                text = text,
-                modifier = Modifier.weight(1f),
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.titleSmall
+        leftIcon?.let {
+            Icon(
+                painter = painterResource(id = it),
+                contentDescription = null
             )
-            rightIcon?.let {
-                Icon(
-                    painter = painterResource(id = it),
-                    contentDescription = null
-                )
-            }
+            Spacer(modifier = Modifier.weight(0.5f))
+        }
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleSmall
+        )
+        rightIcon?.let {
+            Spacer(modifier = Modifier.weight(0.5f))
+            Icon(
+                painter = painterResource(id = it),
+                contentDescription = null
+            )
         }
     }
 }
