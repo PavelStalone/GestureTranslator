809d70426d66818881a9d15e2e7065cd0fcce238
Gleb Poroshin <Poroshin.info@yandex.ru>
2023-12-04 00:23:00 +0300

    #113 - Smal fixes to make it works :) I'm stupid and forgot to add di to endpoints module, sory

--- network/src/main/kotlin/com/ortin/gesturetranslator/network/di/NetworkEndpointsModule.kt
+++ network/src/main/kotlin/com/ortin/gesturetranslator/network/di/NetworkEndpointsModule.kt
@@ -1,12 +1,24 @@
 package com.ortin.gesturetranslator.network.di
 
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Named
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
-@Singleton
-@Named("HuggingFace")
-@Provides
-fun provideHuggingFaceHost(): String {
-    return "https://api-inference.huggingface.co/models/AccessAndrei/tired"
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class EndpointUrl(val qualifier: String)
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkEndpointsModule {
+
+    @Singleton
+    @Named("HuggingFace")
+    @Provides
+    fun provideHuggingFaceHost(): String = "https://api-inference.huggingface.co/models/AccessAndrei/tired"
+
 }
