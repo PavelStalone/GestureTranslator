1a50f450423c123c7a9187818841338a9158e162
Nikita Savvin <nik.savvin.2004@mail.ru>
2023-12-04 22:55:05 +0300

    #137 - Add KDoc and delete extra file

--- network/src/main/kotlin/com/ortin/gesturetranslator/network/models/NetworkResponse.kt
+++ network/src/main/kotlin/com/ortin/gesturetranslator/network/models/NetworkResponse.kt
@@ -1,7 +1,17 @@
 package com.ortin.gesturetranslator.network.models
 
+/**
+ * The class responsible for storing and transmitting responses from the API
+ */
 sealed class NetworkResponse<out T> {
+
+    /**
+     * Data class, which represents a successful response from the API with a body field of type T
+     */
     data class Success<T>(val body: T) : NetworkResponse<T>()
 
+    /**
+     * Data class, which represents an error with an error field of type Throwable to store any type of error information
+     */
     data class Error(val throwable: Throwable?) : NetworkResponse<Nothing>()
 }
