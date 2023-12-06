fe981056a7be045b80c6a8e4f32d0d224c80d263
Pavel Shoplik <pavel.shoplik@yandex.ru>
2023-12-06 19:22:15 +0300

    #114 - Refactor code in :app

--- app/src/main/kotlin/com/ortin/gesturetranslator/app/presentation/GestureTranslatorScreenFlow.kt
+++ app/src/main/kotlin/com/ortin/gesturetranslator/app/presentation/GestureTranslatorScreenFlow.kt
@@ -2,14 +2,12 @@ package com.ortin.gesturetranslator.app.presentation
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.ortin.gesturetranslator.ui.navigation.MainApplicationScreenFlow
-import com.ortin.gesturetranslator.ui.theme.GestureTranslatorTheme
 
 @Composable
 fun GestureTranslatorScreenFlow(
@@ -21,33 +19,24 @@ fun GestureTranslatorScreenFlow(
     NavHost(
         navController = navController,
         modifier = modifier.padding(paddingValues),
-        startDestination = GestureTranslatorScreen.AuthScreenFlow.route
+        startDestination = GestureTranslatorScreenRoutes.AuthScreenFlow.route
     ) {
-
         /**
          * Authorization screen flow
          */
         composable(
-            route = GestureTranslatorScreen.AuthScreenFlow.route
+            route = GestureTranslatorScreenRoutes.AuthScreenFlow.route
         ) {
-            GestureTranslatorTheme {
-                Surface {
-                    // TODO: Add @Composable func for AuthScreen flow
-                }
-            }
+            // TODO: Add @Composable func for AuthScreen flow
         }
 
         /**
          * Main application screen flow
          */
         composable(
-            route = GestureTranslatorScreen.MainApplicationScreenFlow.route
+            route = GestureTranslatorScreenRoutes.MainApplicationScreenFlow.route
         ) {
-            GestureTranslatorTheme {
-                Surface {
-                    MainApplicationScreenFlow()
-                }
-            }
+            MainApplicationScreenFlow()
         }
     }
 }
--- app/src/main/kotlin/com/ortin/gesturetranslator/app/presentation/GestureTranslatorScreenRoutes.kt
+++ app/src/main/kotlin/com/ortin/gesturetranslator/app/presentation/GestureTranslatorScreenRoutes.kt
@@ -5,25 +5,25 @@ import androidx.compose.runtime.Immutable
 /**
  * Sealed class representing the different screens in the Digital Office app
  *
- * @property route Route associated with the screen
+ * @property route route associated with the screen
  */
 @Immutable
-sealed class GestureTranslatorScreen(val route: String) {
+sealed class GestureTranslatorScreenRoutes(val route: String) {
 
     /**
      * Screens for user authentication
      */
-    data object AuthScreenFlow : GestureTranslatorScreen("Auth")
+    data object AuthScreenFlow : GestureTranslatorScreenRoutes("Auth")
 
     /**
      * Screens for viewing the main features of the application
      */
-    data object MainApplicationScreenFlow : GestureTranslatorScreen("MainApplicationScreen")
+    data object MainApplicationScreenFlow : GestureTranslatorScreenRoutes("MainApplicationScreen")
 
     /**
      * Builds the route string with the provided arguments
      *
-     * @param args Arguments to be appended to the route string
+     * @param args arguments to be appended to the route string
      * @return Constructed route string with the supplied arguments
      */
     fun withArgs(vararg args: String): String {
