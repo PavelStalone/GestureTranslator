26db1be644cf947607ea26b09ad5181ab6516a34
Pavel Shoplik <pavel.shoplik@yandex.ru>
2023-12-06 18:51:33 +0300

    #145 - Removed imports

--- ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/RecognizedLetter.kt
+++ ui/src/main/kotlin/com/ortin/gesturetranslator/ui/components/RecognizedLetter.kt
@@ -2,7 +2,6 @@ package com.ortin.gesturetranslator.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,11 +10,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun RecognizedLetter(
