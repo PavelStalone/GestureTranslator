1db8ee4f4a87ec0bf4ae9042d50ef20561e536d8
Pavel Shoplik <pavel.shoplik@yandex.ru>
2023-12-06 20:21:21 +0300

    #147 - Added modules

---
+++ screens/ui-main/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.com.ortin.gesturetranslator.android.library)
+    alias(libs.plugins.com.ortin.gesturetranslator.android.library.compose)
+}
+
+android {
+    namespace = "com.ortin.gesturetranslator.main"
+}
+
+dependencies {
+    /**
+     * Core implementations
+     */
+    implementation(libs.androidx.core.ktx)
+
+    /**
+     * Modules implementations
+     */
+    implementation(project(":domain"))
+
+    /**
+     * Navigation dependencies
+     */
+    implementation(libs.androidx.navigation.compose)
+}
