46fa95dd99ef27d6ddc4c3aa5e5e655f8a65883f
Gleb Poroshin <Poroshin.info@yandex.ru>
2023-12-08 00:00:06 +0300

    #144 - remove withContext from usecase

--- domain/src/main/kotlin/com/ortin/gesturetranslator/domain/usecases/CorrectTextUseCase.kt
+++ domain/src/main/kotlin/com/ortin/gesturetranslator/domain/usecases/CorrectTextUseCase.kt
@@ -1,25 +1,13 @@
 package com.ortin.gesturetranslator.domain.usecases
 
-import com.ortin.gesturetranslator.domain.di.Dispatcher
-import com.ortin.gesturetranslator.domain.di.GtDispatchers
-import com.ortin.gesturetranslator.domain.models.CorrectedTextModel
-import com.ortin.gesturetranslator.domain.models.NetworkResponse
 import com.ortin.gesturetranslator.domain.models.RecognizedTextModel
 import com.ortin.gesturetranslator.domain.repository.AutocorrectTextRepository
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class CorrectTextUseCase @Inject constructor(
     private val autocorrectTextRepository: AutocorrectTextRepository,
-    @Dispatcher(GtDispatchers.IO) private val dispatcher: CoroutineDispatcher
 ) {
-    suspend fun invoke(data: RecognizedTextModel) = withContext(dispatcher) {
-        return@withContext autocorrectTextRepository.correctText(data)
-    }
+    suspend fun invoke(data: RecognizedTextModel) = autocorrectTextRepository.correctText(data)
 }
