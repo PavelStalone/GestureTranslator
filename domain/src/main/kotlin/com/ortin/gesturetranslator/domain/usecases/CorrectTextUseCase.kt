c4b2da75db4b57be7e5a3d5fef4e5e429cc5989f
Gleb Poroshin <Poroshin.info@yandex.ru>
2023-12-07 21:47:20 +0300

    #144 - I have no idea for name this commit, but I change usecase and add empty line

--- domain/src/main/kotlin/com/ortin/gesturetranslator/domain/usecases/CorrectTextUseCase.kt
+++ domain/src/main/kotlin/com/ortin/gesturetranslator/domain/usecases/CorrectTextUseCase.kt
@@ -1,21 +1,23 @@
 package com.ortin.gesturetranslator.domain.usecases
 
+import com.ortin.gesturetranslator.domain.di.Dispatcher
+import com.ortin.gesturetranslator.domain.di.GtDispatchers
 import com.ortin.gesturetranslator.domain.models.CorrectedTextModel
 import com.ortin.gesturetranslator.domain.models.NetworkResponse
 import com.ortin.gesturetranslator.domain.models.RecognizedTextModel
 import com.ortin.gesturetranslator.domain.repository.AutocorrectTextRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CorrectTextUseCase @Inject constructor(
-    private val autocorrectTextRepository: AutocorrectTextRepository
-) : UseCase<RecognizedTextModel, NetworkResponse<CorrectedTextModel>> {
-
-    override operator fun invoke(data: RecognizedTextModel) :NetworkResponse<CorrectedTextModel>  = runBlocking {
-        withContext(Dispatchers.IO) {
-            return@withContext autocorrectTextRepository.correctText(data)
-        }
+    private val autocorrectTextRepository: AutocorrectTextRepository,
+    @Dispatcher(GtDispatchers.IO) private val dispatcher: CoroutineDispatcher
+) {
+    suspend fun invoke(data: RecognizedTextModel) = withContext(dispatcher) {
+        return@withContext autocorrectTextRepository.correctText(data)
     }
 }
